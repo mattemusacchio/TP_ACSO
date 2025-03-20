aab5d4f4bf38152edeb6027b179e8924f5b5ce3a
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-03-20 13:27:51 -0300

    borre cosas peleles

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -4,51 +4,7 @@
 #include <stdint.h>
 #include "shell.h"
 
-
-
-/* Máscaras útiles para extraer campos de instrucciones */
-#define OPCODE_MASK      0xFFFFFFFF
-#define COND_MASK        0xF0000000
-#define OP_MASK          0x1F000000
-#define RD_MASK          0x0000001F  /* Bits 0-4 */
-#define RN_MASK          0x000003E0  /* Bits 5-9 */
-#define RM_MASK          0x001F0000  /* Bits 16-20 */
-#define IMM12_MASK       0x003FFC00  /* Bits 10-21 */
-#define IMM9_MASK        0x001FF000  /* Bits 12-20 */
-#define IMM26_MASK       0x03FFFFFF  /* Bits 0-25 */
-#define IMM19_MASK       0x00FFFFE0  /* Bits 5-23 */
-#define IMM16_MASK       0x001FFFE0  /* Bits 5-20 */
-#define SHAMT_MASK       0x00C00000  /* Bits 22-23 */
-#define SHAMTW_MASK      0x0000FC00  /* Bits 10-15 (para LSL/LSR) */
-#define N_MASK           0x00200000  /* Bit 21 */
-#define ADDR_MASK        0x000003E0  /* Bits 5-9 */
-#define SHIFT_MASK       0x00C00000  /* Bits 22-23 */
-
-/* Valores de desplazamiento para extraer campos */
-#define RD_SHIFT         0
-#define RN_SHIFT         5
-#define RM_SHIFT         16
-#define IMM12_SHIFT      10
-#define IMM9_SHIFT       12
-#define IMM26_SHIFT      0
-#define IMM19_SHIFT      5
-#define IMM16_SHIFT      5
-#define SHAMT_SHIFT      22
-#define SHAMTW_SHIFT     10
-#define N_SHIFT          21
-#define ADDR_SHIFT       0
-
-
-// /* Códigos de condición para B.cond */
-// #define EQ               0x0
-// #define NE               0x1
-// #define GT               0xC
-// #define LT               0xB
-// #define GE               0xA
-// #define LE               0xD
-
-
-/* Códigos de operación (opcodes) */
+// opcodes
 #define ADD_EXT_OP  0b10001011001
 #define ADD_IMM_OP  0b10010001
 #define ADDS_EXT_OP 0b10101011000
